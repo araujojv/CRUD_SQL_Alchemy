e227d1d847d9356964179d3e01c049f3dfb4cfe8
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-03-12 11:03:57 -0300

    feat- BD sqlite

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from pathlib import Path
 from sqlalchemy import create_engine, String, Boolean
 from sqlalchemy.orm import DeclarativeBase, Mapped,mapped_column
 
+
+
+pasta_atual = Path(__file__).parent
+PATH_TO_BD = pasta_atual/'bd_usuarios.sqlite'
+
 class Base(DeclarativeBase):
     pass
-
 
 class Usuario(Base):
     __tablename__ = "usuarios"
@@ -17,3 +22,5 @@ class Usuario(Base):
     def __repr__(self):
         return f"Usuario({self.id=},{self.nome=})"
     
+    
+engine = create_engine(f"sqlite:///{PATH_TO_BD}")
